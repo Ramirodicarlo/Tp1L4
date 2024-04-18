ac9427836ba7e1e1abe43c894a76b1986ec76e79
Ramiro Di Carlo <ramirodicarlo2@gmail.com>
2024-04-17 21:40:42 -0300

    ,

---
+++ Services/Interfaces/IToDoService.cs
@@ -0,0 +1,15 @@
+﻿using ErrorOr;
+using System.Collections.Generic;
+using TODOLIST.Data.Entities;
+
+namespace TODOLIST.Services.Interfaces
+{
+    public interface IToDoService
+    {
+        List<ToDo> GetAllToDos();
+        ToDo? GetTodoById(int todoId);
+        ToDo CreateTodo(ToDo toDo);
+        ToDo? UpdateTodo(int todoId, ToDo updatedTodo);
+        bool DeleteTodo(int todoId);
+    }
+}
---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TODOLIST.Data.Entities;
+using TODOLIST.Data.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace TODOLIST.Services.Interfaces
+{
+    public interface IUserService
+    {
+        List<User> GetAllUsers();
+        User? GetUserById(int userId);
+        User CreateUser(User user);
+        User? UpdateUser(int userId, User updatedUser);
+        bool DeleteUser(int userId);
+        BaseResponse ValidateUser(string email);
+
+        User? GetUserByEmail(string email);
+
+    }
+}
